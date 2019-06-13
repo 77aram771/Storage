6fdfe0406be61e2aa08254408c27016ccd76cef0
77aram771 <77aram39@gmail.com>
2019-06-13 18:52:36 +0400

    Add Full Site

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,42 @@
 import 'babel-polyfill';
 import './../sass/styles.scss';
 
+// Menu Event
+
+const Menu = document.querySelector('.header_nav_burger_menu');
+const Menu_button = document.querySelector('.header_nav_burger');
+const Menu_button_close = document.querySelector('.header_nav_burger_close');
+let bool = false;
+
+
+
+Menu_button_close.addEventListener('click', () => {
+
+    if (bool) {
+
+        console.log(bool)
+    }
+    else {
+        Menu.style.cssText = 'right: -500px';
+        bool = true;
+        console.log(bool)
+    }
+    bool = true;
+});
+
+Menu_button.addEventListener('click', () => {
+
+    if (bool) {
+        Menu.style.cssText = 'right: -500px';
+        console.log(bool)
+    }
+    else {
+        Menu.style.cssText = 'right: 0';
+        console.log(bool)
+    }
+    bool = false;
+});
+
+
+
+
